dde30b8f0ff2f052f078f2183ba3f6a3536267bd
gssdselv <77834652+gssdselv@users.noreply.github.com>
2021-01-25 11:49:17 +0530

    Delete Client directory

--- Client/APT-UI/src/environments/environment.dev.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-
-  CLIENT_ID : '0oavtfn4dl23rctdQ0h7',
-  ISSUER : ' https://flexdev.oktapreview.com/oauth2/default',
-  LOGIN_REDIRECT_URI : 'http://localhost:4200/callback',
-  LOGOUT_REDIRECT_URI : 'http://localhost:4200/'
-};
